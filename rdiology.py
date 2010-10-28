8ac18e2687092e4dcd3f4761bd530143feadc3d6
Ian McKellar <ian@mckellar.org>
2010-10-27 22:48:53 -0700

    do something useful, make it work on appspot.com

--- rdiology.py
+++ rdiology.py
@@ -1,8 +1,5 @@
 CONSUMER_TOKEN='vrun8954fry59w7297mr5ux5'
 CONSUMER_SECRET='MEsUjbamFt'
-REQUEST_TOKEN='http://rdio.api.mashery.com/oauth/request_token'
-ACCESS_TOKEN='http://rdio.api.mashery.com/oauth/access_token'
-API='http://rdio.api.mashery.com/api/1/'
 
 
 import sys, os, urlparse, logging
@@ -16,7 +13,9 @@ import oauth2 as oauth
 import wsgiref, urllib
 
 import cookies
-import json
+from django.utils import simplejson as json
+
+from cgi import parse_qsl
 
 class Cookies(cookies.Cookies):
     def __getitem__(self, key):
@@ -24,80 +23,140 @@ class Cookies(cookies.Cookies):
     def __setitem__(self, key, item):
         cookies.Cookies.__setitem__(self, key, urllib.quote(json.dumps(item)))
 
-class RdioClient(oauth.Client):
-    def __init__(self, consumer):
-        oauth.Client.__init__(self, consumer)
-
 consumer = oauth.Consumer(CONSUMER_TOKEN, CONSUMER_SECRET)
-client = RdioClient(consumer)
+client = oauth.Client(consumer)
 
 import httplib2
 httplib2.debuglevel = 666
+
+class Rdio(object):
+    REQUEST_TOKEN='http://rdio.api.mashery.com/oauth/request_token'
+    ACCESS_TOKEN='http://rdio.api.mashery.com/oauth/access_token'
+    API='http://rdio.api.mashery.com/api/1/'
+    def __init__(self, consumer_token, consumer_secret, data_store):
+        self.__consumer = oauth.Consumer(consumer_token, consumer_secret)
+        self.__data_store = data_store
+
+
+    def authenticating(self):
+        return self.__data_store.has_key('request_token')
+
+
+    def authenticated(self):
+        return self.__data_store.has_key('access_token')
+
+
+    def begin_authentication(self, callback_url):
+        assert not self.authenticating() and not self.authenticated()
+        
+        resp, content = self.__client().request(Rdio.REQUEST_TOKEN, 'POST',
+                                       urllib.urlencode({'oauth_callback':callback_url}))
+        if resp['status'] != '200':
+            raise Exception("Invalid response %s." % resp['status'])
+        request_token = dict(parse_qsl(content))
+        self.__data_store['request_token'] = request_token
+        return (request_token['login_url']+'?oauth_token='+request_token['oauth_token'])
+
+
+    def complete_authentication(self, oauth_verifier):
+        assert self.authenticating() and not self.authenticated()
+
+        client = self.__client(oauth_verifier)
+        resp, content = client.request(Rdio.ACCESS_TOKEN, "POST")
+        if resp['status'] != '200':
+            raise Exception("Invalid response %s." % resp['status'])
+        access_token = dict(parse_qsl(content))
+        self.__data_store['access_token'] = access_token
+        del self.__data_store['request_token']
+
+        
+    def logout(self):
+        if self.authenticating():
+            del self.__data_store['request_token']
+        if self.authenticated():
+            del self.__data_store['access_token']
+
+
+    def __client(self, oauth_verifier=None):
+        token = None
+        if self.authenticated():
+            at = self.__data_store['access_token']
+            token = oauth.Token(at['oauth_token'], at['oauth_token_secret'])
+        elif self.authenticating():
+            rt = self.__data_store['request_token']
+            token = oauth.Token(rt['oauth_token'], rt['oauth_token_secret'])
+        if token is not None and oauth_verifier is not None:
+            token.set_verifier(oauth_verifier)
+        return oauth.Client(self.__consumer, token)
+
+    def call(self, method, **args):
+        args['method'] = method
+        client = self.__client()
+        resp, content = client.request(Rdio.API, 'POST', urllib.urlencode(args))
+        if resp['status'] != '200':
+            raise Exception("Invalid response %s." % resp['status'])
+        return json.loads(content)
 
 
 def template(name, template_values={}):
     return _template.render(os.path.join(os.path.dirname(__file__), 'templates', name), template_values)
 
-class MainPage(webapp.RequestHandler):
+
+class RdioRequestHandler(webapp.RequestHandler):
+    __cached_rdio = None
+    def rdio(self):
+        if (self.__cached_rdio is None):
+            self.__cached_rdio = Rdio(CONSUMER_TOKEN, CONSUMER_SECRET, Cookies(self))
+        return self.__cached_rdio
+
+
+class MainPage(RdioRequestHandler):
     def get(self):
-        self.response.out.write(template('index.html'))
+        rdio = self.rdio()
+        self.response.out.write(template('index.html', {
+            'authenticated': rdio.authenticated(),
+        }))
 
 
-class AuthRequest(webapp.RequestHandler):
+class AuthPage(RdioRequestHandler):
     def get(self):
-        # obtain a request token
-        callback_url = wsgiref.util.request_uri(self.request.environ)
-        callback_url = callback_url.replace('/authrequest', '/authcallback')
+        rdio = self.rdio()
 
-        logging.info(callback_url)
+        assert not rdio.authenticated()
 
-        resp, content = client.request(REQUEST_TOKEN, 'POST',
-                                       urllib.urlencode({'oauth_callback':callback_url}))
-
-        if resp['status'] != '200':
-            raise Exception("Invalid response %s." % resp['status'])
-        request_token = dict(urlparse.parse_qsl(content))
-
-        cookies = Cookies(self)
-        cookies['request_token'] = request_token
-        self.redirect(request_token['login_url']+'?oauth_token='+request_token['oauth_token'])
+        if not rdio.authenticating():
+            # obtain a request token
+            callback_url = wsgiref.util.request_uri(self.request.environ)
+            #callback_url = callback_url.replace('/authrequest', '/authcallback')
+            self.redirect(self.rdio().begin_authentication(callback_url))
+        else:
+            self.rdio().complete_authentication(self.request.get('oauth_verifier'))
+            self.redirect('/')
 
 
-class AuthCallback(webapp.RequestHandler):
+class LogoutPage(RdioRequestHandler):
     def get(self):
-        self.response.out.write('word up')
-        verifier = self.request.get('oauth_verifier')
-        cookies = Cookies(self)
-        request_token = cookies['request_token']
-        token = oauth.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
-        token.set_verifier(verifier)
-
-        client = oauth.Client(consumer, token)
-        resp, content = client.request(ACCESS_TOKEN, "POST")
-        access_token = dict(urlparse.parse_qsl(content))
-        cookies['access_token'] = access_token
-
-        client = oauth.Client(consumer, oauth.Token(access_token['oauth_token'], access_token['oauth_token_secret']))
+        self.rdio().logout()
+        self.redirect('/')
 
 
-        apicall = {
-            'method': 'getHeavyRotationForUser',
-            'id': 13,
-            'type': 'albums',
-            'scope': 'friends',
-            'timeframe': 'weighted',
-            'attempt_everyone': 'true'
-        }
+class HeavyRotationPage(RdioRequestHandler):
+    def get(self):
+        heavy_rotation = self.rdio().call('getHeavyRotationForUser', id= 13, type = 'albums', scope = 'friends', timeframe = 'weighted', attempt_everyone = 'true')
 
-        resp, content = client.request(API, 'POST', urllib.urlencode(apicall))
-        self.response.out.write(`resp`)
-        self.response.out.write(`content`)
+        self.response.out.write(template('heavy.html', {
+            'albums': heavy_rotation['result']['items']
+        }))
+        from pprint import pprint
+        pprint(heavy_rotation, self.response.out)
+
 
 if __name__ == '__main__':
     from google.appengine.ext.webapp.util import run_wsgi_app
     app = webapp.WSGIApplication([
-                ('/authrequest', AuthRequest),
-                ('/authcallback', AuthCallback),
+                ('/auth', AuthPage),
+                ('/logout', LogoutPage),
+                ('/heavy', HeavyRotationPage),
                 ('/', MainPage),
         ],
         debug=True)
